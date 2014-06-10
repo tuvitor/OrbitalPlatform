bc5c770ce9647002ac4c564d56541070c90d1294
James <jtraverjr@gmail.com>
2014-06-10 00:30:05 -0500

    last one for the night

--- models/permissionsModel.js
+++ models/permissionsModel.js
@@ -1,3 +1,13 @@
 /**
  * Created by codeforcoffee on 6/9/14.
  */
+App.Models.PermissionsModel = Backbone.Model.extend({
+
+    Roles: [ 'admin', 'moderator', 'user', 'lurker' ],
+    PagePermissions: {
+        readAccess: null,
+        writeAccess: null,
+        banned: null
+    }
+
+});
